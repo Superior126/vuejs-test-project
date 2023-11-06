eb3b124dbe6e90b871e07438f291306e95629004
Superior125 <90008321+Superior126@users.noreply.github.com>
2023-11-06 19:23:36 +0000

    Setup Project

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+  // import Styles
+  import './css/main.css';
+
+  // Import Components
+  import TestComponent from './components/TestComponent.vue';
+</script>
+
+<template>
+    <h1>Test</h1>
+    <TestComponent :number="4" />
+</template>
---
+++ src/components/TestComponent.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>Test Component {{ number }}</h1> 
+</template>
+
+<script>
+    // Export props for this component
+    export default {
+    props: ['number']
+    }
+</script>
