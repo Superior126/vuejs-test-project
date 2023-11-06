6ff27d050f4f61e893ece47428824c5c66b23fdc
Superior125 <90008321+Superior126@users.noreply.github.com>
2023-11-06 15:30:22 -0500

    Updates

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,11 @@
 
   // Import Components
   import TestComponent from './components/TestComponent.vue';
+  import Count from './components/Count.vue';
 </script>
 
 <template>
     <h1>Test</h1>
     <TestComponent :number="4" />
+    <Count />
 </template>
---
+++ src/components/Count.vue
@@ -0,0 +1,19 @@
+<script setup>
+    // Import Libraries 
+    import { ref } from 'vue';
+</script>
+
+<script>
+    // Declare count ref
+    const count = ref(0); 
+
+    // Function to update count
+    function update_count() {
+        count.value++ 
+    }
+</script>
+
+<template>
+    <h1>Count: {{ count }}</h1>
+    <button @click="update_count"> +1 </button>
+</template>
